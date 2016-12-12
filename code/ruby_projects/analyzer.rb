fc5e5c100153ba6137c147fc83e184fd441a2ab2
Leandro <leandromarte@gmail.com>
2016-12-12 15:49:41 +0000

    Analyzer 2

--- code/ruby_projects/analyzer.rb
+++ code/ruby_projects/analyzer.rb
@@ -9,3 +9,14 @@ puts "Your last name has #{last_name.length} characters in it"
 
 full_name = first_name + " " + last_name
 puts "Your name in reverse reads #{full_name.reverse}"
+
+puts "Please enter your first number"
+first_number = gets.chomp
+
+puts "Please enter your second number"
+second_number = gets.chomp
+
+puts "The first number multiplied by the second number is: #{first_number.to_i * second_number.to_i}"
+puts "The first number divided by the second number is: #{first_number.to_f / second_number.to_f}"
+puts "The first number subtracted from the second number is: #{second_number.to_i - first_number.to_i}"
+puts "The first number mod the second number is: #{first_number.to_i % second_number.to_i}"
