03773cbb7da04dac6d5db2d908a700a433660d8b
Leandro <leandromarte@gmail.com>
2016-12-14 17:53:58 +0000

    Playground

--- code/ruby_projects/playground.rb
+++ code/ruby_projects/playground.rb
@@ -3,5 +3,6 @@ first_name = gets.chomp
 
 puts "welcome to the playground #{first_name}"
 
-hash = {a: 1, b: 2, c: 3, h: 8, "q" => 0}
+#test which object types can be used as key in hashes
+hash = {a: 1, b: 2, c: 3, h: 8, "q" => 0, 2 => "1", 1.5 => 99}
 puts hash
