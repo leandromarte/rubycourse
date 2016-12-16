6c0ecfb38c058a9954a5eda7a45d6e1ef5df75d3
Leandro <leandromarte@gmail.com>
2016-12-16 16:04:58 +0000

    User class

--- code/ruby_projects/user.rb
+++ code/ruby_projects/user.rb
@@ -1,5 +1,14 @@
 class User
-
-
-
+  attr_accessor :name, :email
+  
+  def initialize(name, email)
+    @name = name
+    @email = email
+  end
 end
+
+u = User.new("Juca", "juca@juca.com")
+u1 = User.new("Jao", "jao@juca.com")
+
+
+puts u1.name + " " + u1.email
